26e63c068128adb4868aaeaba9cee23718591782
Terry <terry.otieno@student.moringaschool.com>
2024-07-13 23:27:34 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+let welcomeMessage = 'Welcome to Flatbook, ' + 'Grace Hopper!';
+
+console.log(welcomeMessage);
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + 'GRACE HOPPER!';
+
+console.log(excitedWelcomeMessage);
+
+const shortGreeting = 'Welcome, G!';
+
+console.log(shortGreeting);
